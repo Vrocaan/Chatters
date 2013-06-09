591465631cebca3a8ffc4dfe6d53312da8b70068
Stephen Badger <stephen.badger@gmail.com>
2013-06-09 03:06:38 +0100

    Adds the beginning of a Chatters schema.

---
+++ schema/1.0.0/schema.sql
@@ -0,0 +1,43 @@
+CREATE TABLE USER_SETTINGS (
+	ckey VARCHAR(255) NOT NULL,
+	description VARCHAR(2000),
+	interests VARCHAR(2000),
+	age TINYINT,
+	location VARCHAR(255),
+	gender ENUM('Male', 'Female', 'Neuter', 'Plural'),
+	namecolour VARCHAR(6),
+	textcolour VARCHAR(6),
+	backgroundcolour VARCHAR(6),
+	fadecolours VARCHAR(255),
+	showcolours TINYINT,
+	showsmilies TINYINT,
+	showimages TINYINT,
+	onjoin VARCHAR(255),
+	onleave VARCHAR(255),
+	cimsounds TINYINT,
+	volume ENUM('Low', 'Medium', 'High'),
+	showtitle TINYINT,
+	showwelcome TINYINT,
+	showmotd TINYINT,
+	clearonreboot TINYINT,
+	lines SMALLINT,
+	highlightcode TINYINT,
+	windowwidth SMALLINT,
+	windowheight SMALLINT,
+	timeformat ENUM('12', '24'),
+	timeoffset TINYINT,
+	inactivitytimer SMALLINT,
+	autoafkmessage VARCHAR(255),
+	password VARCHAR(64),
+	CONSTRAINT PRIMARY KEY USING HASH (ckey)
+);
+
+CREATE TABLE USER_AUTH (
+	ckey VARCHAR(255) NOT NULL,
+	login VARCHAR(255) NOT NULL,
+	type ENUM('DreamSeeker', 'Web', 'Android'),
+	CONSTRAINT UNIQUE (login),
+	INDEX (login, type),
+	CONSTRAINT PRIMARY KEY USING HASH (ckey),
+	CONSTRAINT FOREIGN KEY (ckey) REFERENCES USER_SETTINGS (ckey) ON DELETE CASCADE ON UPDATE CASCADE
+);
